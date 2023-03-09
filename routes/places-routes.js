9d104b353a2b040c87319aa41fc586723996ebe6
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-09 15:31:32 +0100

    Getting a Place By User ID

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -14,12 +14,33 @@ const DUMMY_PLACES = [
     address: "20 W 34th St., New York, NY 10001, United States",
     creator: "u1",
   },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the tallest sky scrapers in the world!",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    address: "20 W 34th St., New York, NY 10001, United States",
+    creator: "u1",
+  },
 ];
 
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid; // => {pid: "p1"}
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
+  });
+
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
   });
 
   res.json({ place });
