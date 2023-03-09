d14f8a2c24573f83a16105be1393e89064765e03
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-09 19:46:09 +0100

    Adding Controllers

---
+++ controllers/places-controllers.js
@@ -0,0 +1,58 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the tallest sky scrapers in the world!",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    address: "20 W 34th St., New York, NY 10001, United States",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the tallest sky scrapers in the world!",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    address: "20 W 34th St., New York, NY 10001, United States",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid; // => {pid: "p1"}
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
