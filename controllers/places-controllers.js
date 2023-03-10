ab20ea8d04009e0abf440f976d2fb4f88bff0293
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-10 18:01:01 +0100

    Validating Patch Requests & User Routes

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -72,6 +72,13 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -87,6 +94,10 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+    return next(new HttpError("Could not find a place for that id.", 404));
+  }
+
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
 
   res.status(200).json({ message: "Deleted place" });
