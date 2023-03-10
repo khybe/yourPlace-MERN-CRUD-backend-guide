72e20c3befd3033da58526c6c135f5452d2e02ad
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-10 19:10:37 +0100

    Creating nodemon.json file & adding it to gitignore to ignore important keys

--- util/location.js
+++ util/location.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const HttpError = require("../models/http-error");
 
-const API_KEY = "AIzaSyDdCaz4QOQKf5BONdMGKduxv2jxM5kaxIY";
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
   const response = await axios.get(
