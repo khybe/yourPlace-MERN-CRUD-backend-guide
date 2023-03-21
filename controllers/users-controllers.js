a58ecf556a3d0213eb110f7a72b9a49dc04b6580
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-21 19:53:33 +0100

    Authorization on 'Delete Places' and changing error code for Invalid Credentials from 401 to 403

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -114,7 +114,7 @@ const login = async (req, res, next) => {
 
   if (!existingUser) {
     return next(
-      new HttpError("Invalid credentials, could not log you in.", 401)
+      new HttpError("Invalid credentials, could not log you in.", 403)
     );
   }
 
@@ -131,7 +131,7 @@ const login = async (req, res, next) => {
 
   if (!isValidPassword) {
     return next(
-      new HttpError("Invalid credentials, could not log you in.", 401)
+      new HttpError("Invalid credentials, could not log you in.", 403)
     );
   }
 
