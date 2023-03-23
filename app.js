eca3a8f66ffb3a96c666fdc531b664512c7997b2
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-23 22:37:57 +0100

    Added process.env.PORT

--- app.js
+++ app.js
@@ -55,10 +55,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ssnre.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(
-      "https://yourplace-mern-project-with-crud-operations-backend.vercel.app/" ||
-        5000
-    );
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
